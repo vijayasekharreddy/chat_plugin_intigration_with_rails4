b0dab790396b9934c6cd71ba15072a50eb4e9290
Vijay <mallidi.reddy@rsystems.com>
2014-03-07 15:40:39 +0530

    Initial commit

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,8 @@
+class PublicController < ApplicationController
+	def index
+  end
+
+  def home
+  	render layout: false
+  end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyChat::Application.config.secret_key_base = '346b0375aaec75b69f526ea733ace88df287a88aaaa9cf6ac78292fa95ecf0b8fd2ffcb751997d62f4dafc49716ed341a146456803f075b0b6b89ba292d11e21'
